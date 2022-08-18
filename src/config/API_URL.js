b54c13362e04d0a333b142d335a60bc03c8fc5c5
Ardo Al-Jaf <a.aljaf@gmail.com>
2022-08-18 18:02:46 +0100

    config to run from git download

--- src/config/API_URL.js
+++ src/config/API_URL.js
@@ -1,3 +1,3 @@
-// export const API_URL = "http://localhost:6010";
+export const API_URL = "http://localhost:6010";
 
-export const API_URL = "https://api.carbon-offset-simulator.ardalanjaf.com";
+// export const API_URL = "https://api.carbon-offset-simulator.ardalanjaf.com";
